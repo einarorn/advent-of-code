94e1a82a4bc9988ca62b4d5619333594f83748e4
einarornsaltpay <einar.olafsson@saltpay.co>
2021-12-11 16:49:13 +0000

    day1: add implementation for santa sleigh keys dept

--- 2021/day1/submarine.go
+++ 2021/day1/submarine.go
@@ -1,12 +1,21 @@
 package day1
 
 import (
+	"advent-of-code/resource_provider"
 	"fmt"
 	"strconv"
 )
 
 func FindSantaSleighKeysDepth(filename string) int {
-	return -1
+	input, err := resource_provider.ReadAllLines(filename)
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+	measurements := sonarSweep(input)
+	increases := measurementIncreases(measurements)
+
+	return increases
 }
 
 func measurementIncreases(measurements []int) int {
--- 2021/integration_test/day1_integration_test.go
+++ 2021/integration_test/day1_integration_test.go
@@ -10,7 +10,7 @@ func TestSubmarine_FindSantaSleighKeysDepth(t *testing.T) {
 	is := is.New(t)
 
 	t.Run("Given we have sample input When we check keys depth Then we depth has increased by 7", func(t *testing.T) {
-		depth := day1.FindSantaSleighKeysDepth("sample.txt")
+		depth := day1.FindSantaSleighKeysDepth("day1-sample.txt")
 
 		is.Equal(depth, 7)
 	})
