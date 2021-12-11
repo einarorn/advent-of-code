f9a5975e8f94c745cb2d1db8314cbd555c2ada82
einarornsaltpay <einar.olafsson@saltpay.co>
2021-12-11 15:45:21 +0000

    day1: add unit tests for resource provider for read file input all lines

---
+++ resource_provider/read_file.go
@@ -0,0 +1,5 @@
+package resource_provider
+
+func ReadAllLines(filename string) []string {
+	return []string{}
+}
---
+++ resource_provider/read_file_test.go
@@ -0,0 +1,18 @@
+package resource_provider
+
+import (
+	"github.com/matryer/is"
+	"testing"
+)
+
+func TestReadFile_AllLines(t *testing.T) {
+	is := is.New(t)
+
+	t.Run("Given we have a file with 3 lines When we read all lines Then we will return them as string array", func(t *testing.T) {
+		expected := []string{"Line 1", "Line 2", "Line 3"}
+
+		allLines := ReadAllLines("test-all-lines")
+
+		is.Equal(allLines, expected)
+	})
+}
