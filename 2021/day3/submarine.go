562ba06f450290b1d58ce2531de7976edf424575
einarornsaltpay <einar.olafsson@saltpay.co>
2021-12-14 20:30:20 +0000

    day3: add implementation for convert binary to decimal method

--- 2021/day3/submarine.go
+++ 2021/day3/submarine.go
@@ -1,5 +1,20 @@
 package day3
 
+import (
+	"math"
+)
+
 func convertBinaryToNumber(binary string) int {
-	return -1
+	l := len(binary)
+	val := 0
+
+	for i := 0; i < l; i++ {
+		pos := l-i-1
+		if binary[pos:pos+1] == "1" {
+			val += int(math.Pow(2, float64(i)))
+		}
+
+	}
+
+	return val
 }
--- 2021/day3/submarine_test.go
+++ 2021/day3/submarine_test.go
@@ -17,7 +17,7 @@ func TestSubmarine_ConvertBinaryToNumber(t *testing.T) {
 		is.Equal(actual, expected)
 	})
 
-	t.Run("Given we have the binary string 1010 When we evaluate the decimal value Then we return the value 5", func(t *testing.T) {
+	t.Run("Given we have the binary string 0101 When we evaluate the decimal value Then we return the value 5", func(t *testing.T) {
 		binary := "0101"
 		expected := 5
 
@@ -26,8 +26,8 @@ func TestSubmarine_ConvertBinaryToNumber(t *testing.T) {
 		is.Equal(actual, expected)
 	})
 
-	t.Run("Given we have the binary string 000 When we evaluate the decimal value Then we return the value 0", func(t *testing.T) {
-		binary := "000"
+	t.Run("Given we have the binary string 0000 When we evaluate the decimal value Then we return the value 0", func(t *testing.T) {
+		binary := "0000"
 		expected := 0
 
 		actual := convertBinaryToNumber(binary)
@@ -35,7 +35,7 @@ func TestSubmarine_ConvertBinaryToNumber(t *testing.T) {
 		is.Equal(actual, expected)
 	})
 
-	t.Run("Given we have the binary string 000 When we evaluate the decimal value Then we return the value 15", func(t *testing.T) {
+	t.Run("Given we have the binary string 1111 When we evaluate the decimal value Then we return the value 15", func(t *testing.T) {
 		binary := "1111"
 		expected := 15
 
