3bd44ed10f86242cd3189d83d10bcb63caa221a4
einarornsaltpay <einar.olafsson@saltpay.co>
2021-12-14 21:08:24 +0000

    day3: add implementation for most or least common bit method

--- 2021/day3/submarine.go
+++ 2021/day3/submarine.go
@@ -5,19 +5,48 @@ import (
 )
 
 func mostOrLeastCommonBit(report []string, mostCommon bool) string {
-	return ""
+	res := ""
+	size := len(report[0])
+	arr := make([]int, size)
+
+	for _, line := range report {
+		for i := 0; i < size; i++ {
+			if line[i:i+1] == "1" {
+				arr[i] += 1
+			} else {
+				arr[i] -= 1
+			}
+		}
+	}
+
+	bitA := "1"
+	bitB := "0"
+
+	if mostCommon == false {
+		bitA = "0"
+		bitB = "1"
+	}
+
+	for _, count := range arr {
+		if count >= 0 {
+			res += bitA
+		} else {
+			res += bitB
+		}
+	}
+
+	return res
 }
 
 func convertBinaryToNumber(binary string) int {
-	l := len(binary)
 	val := 0
+	l := len(binary)
 
 	for i := 0; i < l; i++ {
 		pos := l-i-1
 		if binary[pos:pos+1] == "1" {
 			val += int(math.Pow(2, float64(i)))
 		}
-
 	}
 
 	return val
