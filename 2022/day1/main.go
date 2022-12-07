9636dfe75fb012d9771d57cb65f4f99d69fd2a81
Einar Olafsson <einarorn79@gmail.com>
2022-12-07 00:37:54 +0000

    day 1 first half

---
+++ 2022/day1/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+
+	"advent-of-code/resource_provider"
+)
+
+type Snacks struct {
+	Elf      int
+	Calories int
+}
+
+func main() {
+	input, _ := resource_provider.ReadAllLines("input.txt")
+
+	var list []Snacks
+	list = append(list, Snacks{Elf: 1, Calories: 0})
+	i := 0
+
+	for _, snack := range input {
+		if snack != "" {
+			calories, _ := strconv.Atoi(snack)
+			list[i].Calories += calories
+		} else {
+			i++
+			list = append(list, Snacks{Elf: i, Calories: 0})
+		}
+	}
+
+	sort.Slice(list, func(i, j int) bool {
+		return list[i].Calories > list[j].Calories
+	})
+
+	fmt.Println(list[0])
+}
